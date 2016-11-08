8bc28738e8ee234103cf2fb6ca2a0437dc7e2c0d
Enterprise Open Source Solution <eossth@enterprises-imac.local>
2016-11-08 08:22:25 +0700

    Initial Commit

---
+++ Yod/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Yod
+//
+//  Created by Enterprise Open Source Solution on 11/8/2559 BE.
+//  Copyright © 2559 Enterprise Open Source Solution. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
